25c915d83e23c8d798930b9bc863b472921e27cc
Yuying Xie <xyy@email.unc.edu>
2012-02-28 11:42:40 -0500

    simulations

---
+++ 02-16-2012.h
@@ -0,0 +1,12 @@
+/*
+ *  02-16-2012.h
+ *  
+ *
+ *  Created by xyy on 2/10/12.
+ *  Copyright 2012 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+############################
+##higher dimensions p= 30###
+############################
